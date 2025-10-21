3e767581be12cef0e8a0a3cc3c3440e8c523eb50
Haley <haleygee@arizona.edu>
2025-10-21 10:41:58 -0700

    remove hard-coded noise length

--- simulate-plot-data.R
+++ simulate-plot-data.R
@@ -4,8 +4,8 @@
 #2025-10-21
 
 # simulate predictor variable
-predictor <- rnorm(n=100)
+predictor <- rnorm(n=200)
 # simulate response variable with noise
-response <- 2 * predictor + rnorm(n=100, sd=0.2)
+response <- 2 * predictor + rnorm(n=length(predictor), sd=0.5)
 # plot the data
 plot(x=predictor, y=response)
